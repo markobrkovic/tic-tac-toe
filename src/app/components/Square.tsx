28eacfab31a9a29355fe372708b9c3b21e48527d
markobrkovic <querry999@gmail.com>
2021-11-02 23:06:59 +0100

    :sparkles: Tic Tac Toe

--- src/app/components/Square.tsx
+++ src/app/components/Square.tsx
@@ -1,7 +1,29 @@
 import React from 'react';
 
-function Square(): JSX.Element {
-  return <button className="square">{/* TODO */}</button>;
+type SquareProps = {
+  value: string | null;
+  onClick: () => void;
+};
+
+function Square(props: SquareProps): JSX.Element {
+  return (
+    <button className="square" onClick={() => props.onClick()}>
+      {props.value}
+    </button>
+  );
 }
+
+// import React, { useState } from 'react'
+
+// function Square(props) {
+//   // `value` is the state and `setValue` is the setter function
+//   const [value, setValue] = useState(null);
+
+//   return (
+//     <button className="square" onClick={() => alert('click')}>
+//       {props.value}
+//     </button>
+//   );
+// }
 
 export default Square;
